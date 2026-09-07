7b6220a23e255af682c27d73950919099cbbbcc3
eza_maeziei9 <rezzamaerelosa22@gmail.com>
2026-09-07 14:18:54 +0800

    COMMIT

--- indexphp1/index.php
+++ indexphp1/index.php
@@ -4,48 +4,49 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Output 1</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css">
     <style>
         body {
             font-family: "Arial";
         }
     </style>
 </head>
-<body>
+<body class="bg-success p-2 text-white">
     <h1>PHP Output No. 1</h1>
     
     <fieldset>
-        <legend>This form uses GET request</legend>
+        <legend class="fst-italic">This form uses GET request</legend>
         <form action="redirect.php" method="GET">
         <table>
             <tr>
                 <td>First Name</td>
                 <td>
-                    <input type="text" name="fname" placeholder="Enter First Name" required />
+                    <input type="text" class="form-control" name="fname" placeholder="Enter First Name" required />
                 </td>
             </tr>
             <tr>
                 <td>Middle Name</td>
                 <td>
-                    <input type="text" name="mname" placeholder="Enter Middle Name" required />
+                    <input type="text" class="form-control" name="mname" placeholder="Enter Middle Name" required />
                 </td>
             </tr>
             <tr>
                 <td>Last Name</td>
                 <td>
-                    <input type="text" name="lname" placeholder="Enter Last Name" required />
+                    <input type="text" class="form-control"  name="lname" placeholder="Enter Last Name" required />
                 </td>
             </tr>
             <tr>
                 <td>Age</td>
                 <td>
-                    <input type="number" name="age" placeholder="Enter Age" required />
+                    <input type="number" class="form-control" name="age" placeholder="Enter Age" required />
                 </td>
             </tr>
             <tr>
                 <td>Gender</td>
                 <td>
                     <label for="gender" class="form-label" ></label>
-                    <select class="form-select" name="gender" required>
+                    <select class="form-select" class="form-control" name="gender" required>
                         <option value="M">Male    </option>
                         <option value="F">Female  </option>
                     </select>
@@ -54,26 +55,26 @@
             <tr>
                 <td>Email</td>
                 <td>
-                    <input type="email" name="email" placeholder="Enter Email" required />
+                    <input type="email" class="form-control" name="email" placeholder="Enter Email" required />
                 </td>
             </tr>
             <tr>
                 <td>Address</td>
                 <td>
-                    <input type="text" name="address" placeholder="Enter Complete Address" required />
+                    <input type="text" class="form-control" name="address" placeholder="Enter Complete Address" required />
                 </td>
             </tr>
             <tr>
                 <td>Contact Number</td>
                 <td>
-                    <input type="tel" name="contact_number" placeholder="Enter Contact Number" maxlength="11" pattern="[0-9]{11}"  required />
+                    <input type="tel" class="form-control"  name="contact_number" placeholder="Enter Contact Number" maxlength="11" pattern="[0-9]{11}"  required />
                 </td>
             </tr>
             <tr>
                 <td></td>
                 <td>
-                    <input type="submit" value="Submit Data">
-                    <input type="reset" value="Cancel">
+                    <input type="submit" class="btn btn-primary" value="Submit Data">
+                    <input type="reset" class="btn btn-danger" value="Cancel">
                 </td>
             </tr>
         </table>
@@ -81,67 +82,66 @@
     </fieldset>
     
     <fieldset style="margin-top: 20px">
-        <legend>This form uses POST request</legend>
+        <legend class="fst-italic">This form uses POST request</legend>
         <form action="redirect.php" method="POST">
         <table>
             <tr>
                 <td>First Name</td>
                 <td>
-                    <input type="text" name="fname" placeholder="Enter First Name" required />
+                    <input type="text" class="form-control" name="fname" placeholder="Enter First Name" required />
                 </td>
             </tr>
             <tr>
                 <td>Middle Name</td>
                 <td>
-                    <input type="text" name="mname" placeholder="Enter Middle Name" required />
+                    <input type="text" class="form-control" name="mname" placeholder="Enter Middle Name" required />
                 </td>
             </tr>
             <tr>
                 <td>Last Name</td>
                 <td>
-                    <input type="text" name="lname" placeholder="Enter Last Name" required />
+                    <input type="text" class="form-control"  name="lname" placeholder="Enter Last Name" required />
                 </td>
             </tr>
             <tr>
                 <td>Age</td>
                 <td>
-                    <input type="number" name="age" placeholder="Enter Age" required />
+                    <input type="number" class="form-control" name="age" placeholder="Enter Age" required />
                 </td>
             </tr>
             <tr>
                 <td>Gender</td>
                 <td>
-                    <label for="gender" class="form-label" required></label>
-                    <select class="form-select" name="gender">
-                        <option value="M">Male</option>
-                        <option value="F">Female</option>
+                    <label for="gender" class="form-label" ></label>
+                    <select class="form-select" class="form-control" name="gender" required>
+                        <option value="M">Male    </option>
+                        <option value="F">Female  </option>
                     </select>
                 </td>
             </tr>
             <tr>
                 <td>Email</td>
                 <td>
-                    <input type="email" name="email" placeholder="Enter Email" required />
+                    <input type="email" class="form-control" name="email" placeholder="Enter Email" required />
                 </td>
             </tr>
             <tr>
                 <td>Address</td>
                 <td>
-                    <input type="text" name="address" placeholder="Enter Complete Address" required />
+                    <input type="text" class="form-control" name="address" placeholder="Enter Complete Address" required />
                 </td>
             </tr>
             <tr>
                 <td>Contact Number</td>
                 <td>
-                    <input type="tel" name="contact_number" placeholder="Enter Contact Number" maxlength="11" pattern="[0-9]{11}"  required />
-                    
+                    <input type="tel" class="form-control"  name="contact_number" placeholder="Enter Contact Number" maxlength="11" pattern="[0-9]{11}"  required />
                 </td>
             </tr>
             <tr>
                 <td></td>
                 <td>
-                    <input type="submit" value="Submit Data">
-                    <input type="reset" value="Cancel">
+                    <input type="submit" class="btn btn-primary" value="Submit Data">
+                    <input type="reset" class="btn btn-danger" value="Cancel">
                 </td>
             </tr>
         </table>
