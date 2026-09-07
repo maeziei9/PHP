f28253b36bd29160cf5858b533140cced91f143a
eza_maeziei9 <rezzamaerelosa22@gmail.com>
2026-09-07 14:39:03 +0800

    commit

--- indexphp2/index2.php
+++ indexphp2/index2.php
@@ -1,12 +1,12 @@
 <?php require './layout/head.php'; ?>
 
 <h1>PHP Output No. 2 (Page 2)</h1>
-<p>This output uses require statement in PHP</p>
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, maiores eos. Commodi quos hic in cupiditate ullam officia exercitationem dignissimos rerum quaerat ipsa fuga repudiandae, dolorum nobis est dicta placeat.
+<p  class="text-bg-info p-3">This output uses require statement in PHP</p>
+<p class="text-bg-light p-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, maiores eos. Commodi quos hic in cupiditate ullam officia exercitationem dignissimos rerum quaerat ipsa fuga repudiandae, dolorum nobis est dicta placeat.
 Tempora optio esse et ad corrupti quod libero dicta fuga rem veritatis? Vel asperiores possimus quam placeat minima minus pariatur blanditiis. Praesentium modi odit iste commodi, quo possimus optio cumque?
 Quia ea beatae expedita totam aperiam eligendi. Asperiores aperiam animi nesciunt rem neque eveniet. Adipisci incidunt hic facilis enim ea reiciendis porro, doloribus sapiente aliquid modi, tempore rem expedita unde?</p>
 
-<a href="./">Go to main Page</a>
+<a href="./" class="nav-link active">Go to main Page</a>
 
 <?php require './layout/foot.php'; ?>
 
